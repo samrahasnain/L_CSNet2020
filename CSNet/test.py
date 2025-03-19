ec28e3e4408dd9e5e147f356ab1b7c2ed42ab6e3
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2025-03-19 12:46:55 +0500

    Update test.py

--- CSNet/test.py
+++ CSNet/test.py
@@ -87,6 +87,8 @@ def test(model, test_datasets, epoch):
                 img = torch.unsqueeze(torch.FloatTensor(img), 0)
                 input_var = torch.autograd.Variable(img)
                 input_var = input_var.cuda()
+                if self.config.cuda:
+                    torch.cuda.synchronize()
                 time_s = time.time()
                 predict = model(input_var)
                 predict = predict[0]
@@ -98,6 +100,8 @@ def test(model, test_datasets, epoch):
                 save_file = os.path.join(sal_save_dir, img_name[0:-4] + '.png')
                 io.imsave(save_file, predict)
                 count += 1
+                if self.config.cuda:
+                    torch.cuda.synchronize()
                 time_e = time.time()
                 print(f'Speed: { (time_e - time_s):.2f} FPS')
         print('Dataset: {}, {} images'.format(dataset, len(img_list)))
